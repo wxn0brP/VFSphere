f8104476e947534f3e0ec86bdb4220da6b492138
wxn0brP <toja.filiper@gmail.com>
2025-09-05 21:32:23 +0200

    feat: update cli

--- src/cli.ts
+++ src/cli.ts
@@ -4,30 +4,31 @@ import { VFS } from "./class";
 const args = process.argv.slice(2);
 const vfs = await new VFS().init();
 
-if (args[0] === "add") {
+const a0 = args[0];
+if (a0 === "add" || a0 === "a") {
     if (!args[1]) throw new Error("Usage: add <file> [mount]");
     const file = args[1];
     const mount = args[2] || "/" + file.split("/").pop();
     const data = await readFile(file);
     await vfs.write(mount, data);
 }
-else if (args[0] === "extract") {
+else if (a0 === "extract" || a0 === "e") {
     if (!args[1]) throw new Error("Usage: extract <mount> [out file]");
     const mount = args[1];
     const file = args[2] || "." + mount; // ./file
     const data = await vfs.read(mount);
     await writeFile(file, data);
 }
-else if (args[0] === "delete") {
+else if (a0 === "delete" || a0 === "rm") {
     if (!args[1]) throw new Error("Usage: delete <mount>");
     await vfs.delete(args[1]);
 }
-else if (args[0] === "read") {
+else if (a0 === "read" || a0 === "cat" || a0 === "r") {
     if (!args[1]) throw new Error("Usage: read <mount>");
     const data = await vfs.read(args[1]);
     process.stdout.write(data);
 }
-else if (args[0] === "list")
+else if (a0 === "list")
     console.log(await vfs.list());
 else
     console.log(`
@@ -37,4 +38,4 @@ Usage:
     delete <mount>
     read <mount>
     list
-`);
+`.trim());
