5ab25d465e36e1b187410e061cd2ed28e4699a1e
wxn0brP <toja.filiper@gmail.com>
2025-10-10 19:33:31 +0200

    feat: json encode

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import * as msgpack from "@msgpack/msgpack";
 
+const devJson = process.env["VFSPHERE_DEV_JSON"] === "true";
+
 function isMsgpackData(collection: string) {
     if (collection === "") return true;
     if (collection.startsWith("%")) return true;
@@ -8,12 +10,12 @@ function isMsgpackData(collection: string) {
 
 export async function encodeData(data: any, collection: string) {
     // console.log("encodeData", collection, data.length);
-    if (isMsgpackData(collection)) return msgpack.encode(data);
+    if (isMsgpackData(collection)) return devJson ? JSON.stringify(data) : msgpack.encode(data);
     return data;
 }
 
 export async function decodeData(data: Buffer, collection: string) {
     // console.log("decodeData", collection, data.length);
-    if (isMsgpackData(collection)) return msgpack.decode(data);
+    if (isMsgpackData(collection)) return devJson ? JSON.parse(data.toString()) : msgpack.decode(data);
     return data;
 }
